12bd26e334876f05e44c8aa1bca66ebfdffb0554
Patrick Smith <patrick@burntcaramel.com>
2017-10-10 14:56:10 +1100

    Add various company queries
    Set operations in Rails: intersections, unions, complement, difference

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,6 +5,27 @@ class CompaniesController < ApplicationController
   # GET /companies.json
   def index
     @companies = Company.all
+
+    @finance_companies = Company.where(industry: 'finance')
+    @productivity_companies = Company.where(industry: 'productivity')
+    @ecommerce_companies = Company.where(industry: 'ecommerce')
+    @utility_companies = Company.where(industry: 'utility')
+
+    # All companies that have relationships to jobs,
+    # and then ensure companies aren’t repeated with `distinct`
+    @companies_hiring = Company.all.joins(:jobs).distinct
+
+    # Companies in finance and also hiring (intersection)
+    @finance_companies_hiring = @finance_companies & @companies_hiring
+
+    # Companies either in the ecommerce or utility industries (union)
+    @union_companies = @ecommerce_companies | @utility_companies
+
+    # Companies with no jobs therefore not hiring (complement)
+    @companies_not_hiring = Company.where.not(id: @companies_hiring)
+
+    # Companies hiring excluding those in finance (difference)
+    @non_finance_companies_hiring = @companies_hiring - @finance_companies
   end
 
   # GET /companies/1
